3275ffe314843c5472e0e2f0a2c482189cc14a2b
Bohdan <grabetsb@gmail.com>
2023-12-31 11:17:25 +0200

    Initial commit. Solution created

---
+++ qpm.e2e/qpm.e2e.tests/BrowserRunner.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Playwright;
+
+namespace qpm.e2e.tests
+{
+    public class BrowserRunner
+    {
+        public IPlaywright? PlaywrightInstance { get; set; }
+        public IBrowser? Browser { get; set; }
+
+        public async Task<IPage> OpenInitPage(string url)
+        {
+            var launchOptions = new BrowserTypeLaunchOptions
+            {
+                //Headless = false,
+                Args = new List<string> { "--start-maximized" },
+                //SlowMo = 50,
+                
+            };
+
+            PlaywrightInstance = await Playwright.CreateAsync();
+
+            Browser = await PlaywrightInstance.Chromium.LaunchAsync(launchOptions);
+
+            var context = await Browser.NewContextAsync(
+                new BrowserNewContextOptions
+                {
+                    ViewportSize = ViewportSize.NoViewport
+                });
+
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync(url);
+            return page;
+        }
+    }
+}
---
+++ qpm.e2e/qpm.e2e.tests/SanityTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace qpm.e2e.tests
+{
+    [TestFixture]
+    public class SanityTests
+    {
+        const string BaseUrl = "https://app.qpm.digital/";
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            // https://temp-mail.org/
+        }
+
+        [Test]
+        public async Task UsersIterationTest()
+        {
+            const string adminName = "hinogir860@vasteron.com";
+            const string password = "6T76vuEjfgs9US5";
+            const string userName = "jiyive1530@ubinert.com";
+
+            var adminBrowser = new BrowserRunner();
+            var adminPage = await LoadStartPage(adminName, password, adminBrowser);
+
+            var userBrowser = new BrowserRunner();
+            var userPage = await LoadStartPage(userName, password, userBrowser);
+            await adminPage.Locator("text=Product increments").ClickAsync();
+            await userPage.Locator("text=Product increments").ClickAsync();
+        }
+
+        private static async Task<IPage> LoadStartPage(string adminName, string password, BrowserRunner adminBrowser)
+        {
+            var adminPage = await adminBrowser.OpenInitPage(BaseUrl);
+
+            await adminPage.Locator("id=signInName").FillAsync(adminName);
+            await adminPage.Locator("id=password").FillAsync(password);
+            await adminPage.Locator("id=next").ClickAsync();
+
+            await adminPage.WaitForLoadStateAsync();
+
+            await adminPage.Locator("text=Product increments").WaitForAsync();
+
+            await Assertions.Expect(adminPage).ToHaveURLAsync($"{BaseUrl}inbox");
+
+            return adminPage;
+        }
+    }
+}
